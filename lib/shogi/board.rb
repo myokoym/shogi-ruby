e41044105675bcd2ad7be1bc40473508dec43e5d
Masafumi Yokoyama <myokoym@gmail.com>
2013-04-26 00:21:22 +0900

    Rename method
    TODO: Integerクラスのメソッドにしたい

--- lib/shogi/board.rb
+++ lib/shogi/board.rb
@@ -127,8 +127,8 @@ module Shogi
     end
 
     def at(place)
-      array_x = shogi_x_to_array_x(place[0].to_i)
-      array_y = shogi_y_to_array_y(place[1].to_i)
+      array_x = to_array_x_from_shogi_x(place[0].to_i)
+      array_y = to_array_y_from_shogi_y(place[1].to_i)
       @position[array_y][array_x]
     end
 
@@ -166,8 +166,8 @@ module Shogi
         before_cell = before_piece
         before_piece = Piece.const_get(before_cell[1..2]).new
       else
-        before_x = shogi_x_to_array_x(csa[1].to_i)
-        before_y = shogi_y_to_array_y(csa[2].to_i)
+        before_x = to_array_x_from_shogi_x(csa[1].to_i)
+        before_y = to_array_y_from_shogi_y(csa[2].to_i)
         before_cell = @position[before_y][before_x]
         if before_cell == ""
           raise MoveError, "Before cell is blank"
@@ -183,7 +183,7 @@ module Shogi
           raise MoveError, "Don't promote: #{before_cell[1..2]} -> #{csa[5..6]}"
           end
 
-          after_y = shogi_y_to_array_y(csa[4].to_i)
+          after_y = to_array_y_from_shogi_y(csa[4].to_i)
           if csa[0] == "+"
             unless after_y < 3 || before_y < 3
               raise_movement_error("Don't promote this move: #{csa}")
@@ -196,8 +196,8 @@ module Shogi
         end
       end
 
-      after_x = shogi_x_to_array_x(csa[3].to_i)
-      after_y = shogi_y_to_array_y(csa[4].to_i)
+      after_x = to_array_x_from_shogi_x(csa[3].to_i)
+      after_y = to_array_y_from_shogi_y(csa[4].to_i)
       after_cell = @position[after_y][after_x]
       if csa[0] == after_cell[0]
         raise MoveError, "Your piece on after cell: #{csa}"
@@ -257,19 +257,19 @@ module Shogi
       end
     end
 
-    def shogi_x_to_array_x(shogi_x)
+    def to_array_x_from_shogi_x(shogi_x)
       9 - shogi_x
     end
 
-    def shogi_y_to_array_y(shogi_y)
+    def to_array_y_from_shogi_y(shogi_y)
       shogi_y - 1
     end
 
-    def array_x_to_shogi_x(array_x)
+    def to_shogi_x_from_array_x(array_x)
       9 - array_x
     end
 
-    def array_y_to_shogi_y(array_y)
+    def to_shogi_y_from_array_y(array_y)
       array_y + 1
     end
   end
