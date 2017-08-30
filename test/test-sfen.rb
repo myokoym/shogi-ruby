256e0a93b145e85a35c536b913b4ebf8e2242ddb
Shigeaki Matsumura <matsu911@gmail.com>
2017-08-30 21:53:56 +0900

    fix test cases for sfen

--- test/test-sfen.rb
+++ test/test-sfen.rb
@@ -4,7 +4,7 @@ require 'stringio'
 class SFENTest < Test::Unit::TestCase
   def test_sfen_1
     @game = Shogi::Game.new
-    assert_equal('lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 0', @game.to_sfen)
+    assert_equal('lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1', @game.to_sfen)
   end
 
   def test_sfen_2
@@ -24,6 +24,6 @@ class SFENTest < Test::Unit::TestCase
     @game.move('+8786FU', :csa)
     @game.move('-8286HI', :csa)
     @game.move('+2434HI', :csa)
-    assert_equal('lnsgk1snl/6gb1/p1pppp2p/6R2/9/1rP6/P2PPPP1P/1BG6/LNS1KGSNL w 3P2p 15', @game.to_sfen)
+    assert_equal('lnsgk1snl/6gb1/p1pppp2p/6R2/9/1rP6/P2PPPP1P/1BG6/LNS1KGSNL w 3P2p 16', @game.to_sfen)
   end
 end
